9179b0389fd80beb701824dec0c8a3e0d7f89e62
yevheniimoskalenko <testjokerqwerty@gmail.com>
2020-06-15 00:03:04 +0300

    install eslint+ prettier and create component TodoList

---
+++ src/Todo/Todo.list.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const styles = {
+  ul: {
+    listStyle: "none",
+  },
+};
+export default function TodoList() {
+  return (
+    <ul style={styles.ul}>
+      <li>1</li>
+      <li>2</li>
+    </ul>
+  );
+}
